063589c9527cd10c624b3d826824435418ad9c3b
jamesfulreader <jamesfulreader@gmail.com>
2024-12-21 16:31:37 -0700

    refactor code to follow solution files create commands.go and handler_user.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,52 +2,67 @@ package config
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 	"path/filepath"
 )
 
-const configFileName = "gatorconfig.json"
+const configFileName = ".gatorconfig.json"
 
 type Config struct {
 	DBURL           string `json:"db_url"`
-	CurrentUserName string `json:"current_user_name,omitempty"`
+	CurrentUserName string `json:"current_user_name"`
 }
 
-func (c *Config) SetUser(username string) error {
-	c.CurrentUserName = username
-	return write(*c)
+func (cfg *Config) SetUser(userName string) error {
+	cfg.CurrentUserName = userName
+	return write(*cfg)
 }
 
 func Read() (Config, error) {
-	config, err := os.ReadFile(getConfigFilePath())
+	fullPath, err := getConfigFilePath()
 	if err != nil {
 		return Config{}, err
 	}
 
-	var data Config
+	file, err := os.Open(fullPath)
+	if err != nil {
+		return Config{}, err
+	}
+	defer file.Close()
 
-	if err := json.Unmarshal(config, &data); err != nil {
-		return Config{}, fmt.Errorf("error unmarshaling due to %s", err)
+	decoder := json.NewDecoder(file)
+	cfg := Config{}
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		return Config{}, err
 	}
 
-	return data, nil
+	return cfg, nil
 }
 
-func getConfigFilePath() string {
-	projectRoot := filepath.Join("cd ./../../gator", configFileName)
-	return projectRoot
+func getConfigFilePath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	fullPath := filepath.Join(home, configFileName)
+	return fullPath, nil
 }
 
-func write(c Config) error {
-	file, err := os.Create(getConfigFilePath())
+func write(cfg Config) error {
+	fullPath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(fullPath)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
 	encoder := json.NewEncoder(file)
-	err = encoder.Encode(c)
+	err = encoder.Encode(cfg)
 	if err != nil {
 		return err
 	}
--- main.go
+++ main.go
@@ -3,22 +3,41 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/jamesfulreader/gator/internal/config"
 )
+
+type state struct {
+	cfg *config.Config
+}
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Printf("read config : %+v\n", cfg)
 
-	err = cfg.SetUser("james")
-
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
+	programState := &state{
+		cfg: &cfg,
 	}
-	fmt.Printf("read config again: %+v\n", cfg)
+
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: cli <command> [args...]")
+		os.Exit(1)
+		return
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
